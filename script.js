d1b2fe15be7b1ec370ef9d025f3e509438a655fe
unknown <mwgoodwin2000@gmail.com>
2022-10-03 22:52:55 -0600

    finished

--- script.js
+++ script.js
@@ -5,11 +5,8 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var specials = "!'#@$%^&*-+.<>;:[]{}()_=?/|";
 
-
-
 // Write password to the #password input
 function generatePassword() {
-var generatedPassword;
   var passwordLength = window.prompt("How long would you like your password?");
   
   if(passwordLength < 8 || passwordLength > 128){
@@ -17,7 +14,42 @@ var generatedPassword;
     return;
   }
 
-  return generatedPassword;
+  var useUpperCase = window.confirm("Would you like to include UPPERCASE characters in your password?");
+  var useLowerCase = window.confirm("Would you like to include LOWERCASE characters in your password?");
+  var useSpecials = window.confirm("Would you like to include SPECIAL characters in your password?");
+  var useNumbers = window.confirm("Would you like to include NUMBER characters in your password?");
+  
+  if(!upperCase && !lowerCase && !specials && !numbers){
+    window.alert("Please select at least one category of criteria or Brent will find you.>:)");
+    return;
+  }
+
+  var passwordChoices = '';
+  var finishedPassword = '';
+
+  if(useUpperCase == true){
+    passwordChoices = passwordChoices + upperCase;
+  }
+
+  if(useLowerCase == true){
+    passwordChoices = passwordChoices + lowerCase;
+  }
+  
+  if(useSpecials == true){
+    passwordChoices = passwordChoices + specials;
+  }
+  
+  if(useNumbers == true){
+    passwordChoices = passwordChoices + numbers;
+  }
+
+  console.log({passwordChoices});
+
+  for (let i = 0; i < passwordLength; i++) {
+    finishedPassword = finishedPassword + passwordChoices.charAt(Math.floor(Math.random() * passwordChoices.length - 1));
+  }
+  
+  return finishedPassword;
 }
 
 function writePassword() {
@@ -25,12 +57,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// return generatedPassword
